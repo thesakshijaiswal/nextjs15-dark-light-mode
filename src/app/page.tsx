0185a99c0228dd35181dd3fbbea573cd77a5b36c
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-08-15 17:35:38 +0530

    feat: implement theme toggle functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-import Image from "next/image";
+import ThemeToggle from "@/components/ThemeToggle";
 
 export default function Home() {
   return (
-    <div className="font-sans flex items-center justify-center min-h-screen p-8 pb-20 gap-3.5 sm:p-20 text-6xl">
-      <span>Hello</span> <span className="text-lime-400">Dev Community...</span>
+    <div className="font-sans flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-3.5 sm:p-20 ">
+      <ThemeToggle />
+      <div className="text-7xl font-bold">
+        <span>Hello</span>{" "}
+        <span className="text-orange-400">Dev Community🔥...</span>
+      </div>
+      <div>
+        Made with 🧡 by{" "}
+        <a
+          href="linkedin.com/in/thesakshijaiswal"
+          className="hover:text-orange-400"
+        >
+          {" "}
+          Sakshi Jaiswal
+        </a>
+      </div>
     </div>
   );
 }
